e2334fe6a9fdfe0aa8810d5bd5a3479b85fe76d1
Juanjo <josejuan250203@gmail.com>
2020-05-01 17:10:27 -0500

    Agregamos más funciones al modelo Chat.py

--- appChat/chat.py
+++ appChat/chat.py
@@ -6,3 +6,15 @@ class Chat:
     def mostrar_chat(self):
 
         print("El comentario dice: "+self.descripcion)
+
+    def mostrar_chat212(self):
+
+        print("El comentario dice: "+self.descripcion)
+
+    def mostrar_chat24(self):
+
+        print("El comentario dice: "+self.descripcion)
+
+    def mostrar_chat23(self):
+
+        print("El comentario dice: "+self.descripcion)
