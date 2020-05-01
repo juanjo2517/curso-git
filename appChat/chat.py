1e4541053a870ce3eb28d054fc0ffbc00a01da45
Juanjo <josejuan250203@gmail.com>
2020-05-01 17:12:01 -0500

    Eliminamos dos funciones innecesarias del modelo Chat.py

--- appChat/chat.py
+++ appChat/chat.py
@@ -10,11 +10,3 @@ class Chat:
     def mostrar_chat212(self):
 
         print("El comentario dice: "+self.descripcion)
-
-    def mostrar_chat24(self):
-
-        print("El comentario dice: "+self.descripcion)
-
-    def mostrar_chat23(self):
-
-        print("El comentario dice: "+self.descripcion)
